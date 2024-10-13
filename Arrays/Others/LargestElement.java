6641b17dd9f58b09ba2c9f647f1fc3a4ee4a3676
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-10-14 01:19:43 +0530

    isSorted method

---
+++ Arrays/Others/LargestElement.java
@@ -0,0 +1,10 @@
+public class LargestElement {
+    static int data[] = {1,3,5,3,7,8};
+    public static void main(String[] args) {
+        largestElement(data);
+    }
+
+    public static int largestElement(int[] data){
+        return 1;
+    }
+}
---
+++ Arrays/Others/SortedOrNot.java
@@ -0,0 +1,17 @@
+public class SortedOrNot {
+    static int[] data = {1,2,3,4,5,6,9};
+    public static void main(String[] args) {
+        System.out.println(isSorted(data));
+    }
+
+    static boolean isSorted(int[] arr){
+        for(int i = 1; i < arr.length ; i++){
+            if(arr[i-1] <= arr[i]){
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
