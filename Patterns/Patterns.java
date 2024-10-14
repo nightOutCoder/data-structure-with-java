1099014777ab9e5759736a12268edcd3b5d7c753
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-10-14 23:48:23 +0530

    patterns

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -7,7 +7,10 @@ public class Patterns {
         //pattern5(5);
         //pattern6(5);
         //pattern7(5);
-        //pattern8(5); /* For pattern 8 call both two method together 7 and 8 */
+        //pattern8(5); 
+        //pattern9(5) 
+        //pattern10(5);
+          pattern11(5);
     }
     public static void pattern1(int n){
         for(int i = 0; i<n ; i++){
@@ -88,6 +91,42 @@ public class Patterns {
             }
             for(int j = 0; j < i; j++){
                 System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern9(int n){
+        pattern7(n);
+        pattern8(n);
+    }
+
+    public static void pattern10(int n){
+        for(int i = 1; i <= 2*n-1; i++){
+
+            int end = i;
+            if(i > n){
+                end = 2*n-i;
+            }
+            for(int j = 1; j <= end; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern11(int n){
+        int start = 1;
+        for(int i = 0; i < n ; i++){
+
+            if(i % 2 == 0){
+                start = 1;
+            } else {
+                start = 0;
+            }
+            for(int j = 0; j <= i; j++){
+                System.out.print(start);
+                start = 1 - start;
             }
             System.out.println();
         }
