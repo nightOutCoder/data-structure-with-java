f96790ed64a68f40927a852b5ed9f67dc3329c82
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-10-22 00:54:06 +0530

    fix: permutation via functional and parameterised way

---
+++ Recursion/Permutation.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class Permutation {
+    public static void main(String a[]){
+        System.out.println(getPermuationViaFunctionalWay(4));
+        System.out.println(getPermuationViaParWay(4,1));
+    }
+
+    /* Via Functional way */
+    public static int getPermuationViaFunctionalWay(int n ){
+        if(n == 0)
+            return 1;
+        else 
+            return n * getPermuationViaFunctionalWay(n-1);
+    }
+
+    /* Via Parameterised way */
+    public static int getPermuationViaParWay(int n, int product){
+        if(n < 1)
+            return product;
+        else 
+            return getPermuationViaParWay(n-1, product*n);
+    }
+}
