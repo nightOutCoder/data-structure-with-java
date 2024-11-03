0913bd2d4dc939a3da87e0da984ff80f27709f09
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-11-03 23:41:29 +0530

    single number - brute force + better + optimized

--- LeetCode/BitManu/LC136.java
+++ LeetCode/BitManu/LC136.java
@@ -3,62 +3,89 @@ package LeetCode.BitManu;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ *  Single Number
+*/
 public class LC136 {
-    static int[] data = {2,2,1};
-    public static void main(String[] args) {
+    static int[] data = { 2, 2, 1 };
 
+    public static void main(String[] args) {
+        int[] result = getViaAdditionalArray(data);
+        for(int i = 0; i < result.length; i++){
+            if(result[i] != -1 && result[i] < 2){
+                System.out.println(data[i]+" came "+result[i] +" times");
+            }
+        }
         System.out.println(getSingleNumberViaMap(data));
         System.out.println(getSingleNumber(data));
     }
 
 
-    public static int getSingleNumberViaMap(int[] inputData){
-    int result = 0;
-
-    Map<Integer, Integer> data = new HashMap<Integer,Integer>();
-
-    /*
-        * O(N * log m) -> m is size of map
+    /**
+     * Time Complexity : O(n2)
+     * Space Complexity : O(n) + O(1) ==> O(n)
     */
-    for(int element : inputData){
-        if(data.containsKey(element)){
-            data.put(element, data.get(element)+1);
-        } else {
-            data.put(element , 1);
+    public static int[] getViaAdditionalArray(int[] inputData) {
+        int[] storeArray = new int[inputData.length];
+        int isVisited = -1;
+
+        for(int i = 0 ; i < inputData.length; i ++){
+            int count = 1; 
+            for(int j = i+1; j < inputData.length; j++){
+                if(inputData[i] == inputData[j]){
+                    count++;
+                    storeArray[j] = isVisited;
+                }
+            }
+            if(storeArray[i] != isVisited){
+                storeArray[i] = count;
+            }
         }
+        return storeArray;
     }
 
-    /*
-        * O(m) - m is size of map
-    */
-    for(Map.Entry<Integer,Integer> i : data.entrySet()){
-        if(i.getValue() < 2)
-        result = i.getKey();
-    }
 
-        /* Total Time : complexity
-         * (N *  log m) + O(m)
+    public static int getSingleNumberViaMap(int[] inputData) {
+        int result = 0;
+        Map<Integer, Integer> data = new HashMap<Integer, Integer>();
+        /*
+         * O(N * log m) -> m is size of map
+         */
+        for (int element : inputData) {
+            if (data.containsKey(element)) {
+                data.put(element, data.get(element) + 1);
+            } else {
+                data.put(element, 1);
+            }
+        }
+        /*
+         * O(m) - m is size of map
+         */
+        for (Map.Entry<Integer, Integer> i : data.entrySet()) {
+            if (i.getValue() < 2)
+                result = i.getKey();
+        }
+        /*
+         * Total Time : complexity
+         * (N * log m) + O(m)
          * 
          * Space : O(m)
-        */
-
-     return result;
-}
-
+         */
+        return result;
+    }
 
     /**
-    * 
-    * TC - O(n) 
-    * SC - O(1)
-    *
-    */
-    public static int getSingleNumber(int[] inputData){
+     * 
+     * TC - O(n)
+     * SC - O(1)
+     *
+     */
+    public static int getSingleNumber(int[] inputData) {
         int xor = 0;
-        for(int element : inputData){
+        for (int element : inputData) {
             xor = xor ^ element;
         }
         return xor;
     }
-
 
 }
