0caa58eb04648bd883d61b22fa8899590f395d3c
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-11-10 23:52:27 +0530

    Pattern

--- Basics/Patterns/Patterns.java
+++ Basics/Patterns/Patterns.java
@@ -10,7 +10,8 @@ public class Patterns {
         //pattern8(5); 
         //pattern9(5) 
         //pattern10(5);
-          pattern11(5);
+        //pattern11(5);
+          pattern12(6);
     }
     public static void pattern1(int n){
         for(int i = 0; i<n ; i++){
@@ -127,6 +128,19 @@ public class Patterns {
             for(int j = 0; j <= i; j++){
                 System.out.print(start);
                 start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+
+    /* ta */
+    public static void pattern12(int n){
+        for(int i = 1; i <=n; i++){
+            for(int j = 1; j <= (n-i); j++){
+                System.out.print(" ");
+            } 
+            for(int j = 1; j<=i;j++){
+                System.out.print(i+" ");
             }
             System.out.println();
         }
